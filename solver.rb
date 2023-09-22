709dfb8c674a0168e934bb5c64c7b151eac9721e
ctokoli <matumasie@gmail.com>
2023-09-22 12:44:52 +0000

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -1,5 +1,14 @@
 # class Solver
 class Solver
-  def factorial
+  def factorial(num)
+    result = 1
+    if [0, 1].include?(num)
+      result
+    end
+    for i in 1..num do
+      result *= i
+    end
+    return result
   end
 end
+
