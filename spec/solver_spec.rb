67880ec08b27d69d9183207cac598ff95054a8dd
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-22 16:42:55 +0300

    Add tests for  reverse methods and method for string reversal

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,11 +1,19 @@
 require_relative '../solver'
 
 describe Solver do
-  describe `#factorial` do
+  describe 'factorial' do
     it 'should return factorial number' do
       facto = Solver.new
       value = facto.factorial(5)
       expect(value).to eql(120)
+    end
+end
+
+describe 'reverse' do
+    it 'should return the reversed string' do
+      solver = Solver.new
+      reversed = solver.reverse('hello')
+      expect(reversed).to eql('olleh')
     end
   end
 end
